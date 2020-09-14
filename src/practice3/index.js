368e46996492f5b9e7ccbc05d057ce31c0bc77f0
EscapedSheep <sfgmdf001@126.com>
2020-09-14 17:19:42 +0800

    [maida] completed all practice

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,9 @@
-export const getPoetry
+export const getPoetry = async () => {
+    try {
+        const response = await fetch("https://v1.jinrishici.com/all.json");
+        const data = await JSON.parse(response.body);
+        return [data.origin, data.author, data.content];
+    } catch (e) {
+        console.log(e);
+    }
+};
